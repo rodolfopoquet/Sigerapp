944eeaf0fb7ac3f4cc2d041c89175379395fdee1
João Vitor <johnbitencourtbr@gmail.com>
2019-03-19 20:35:37 -0300

    19-03-19- Disparo de e-mail de recuperação de senha

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\ResetDeSenha;
 
 class User extends Authenticatable
 {
@@ -27,4 +28,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        
+        $this->notify(new ResetDeSenha($token));
+    }
+
+
+
+
+
+
 }
