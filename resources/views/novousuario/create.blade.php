863c26d13d102e6e0fa9ac2546eb863fa3618fcd
João Vitor <johnbitencourt@gmail.com>
2019-05-20 18:03:54 -0300

    20-05-19- Correções de erros de form

--- resources/views/novousuario/create.blade.php
+++ resources/views/novousuario/create.blade.php
@@ -26,17 +26,8 @@
   <div class="card-header">
 
   </div>
-  <div class="card-body">
-    @if ($errors->any())
-      <div class="alert alert-danger">
-      
-        <ul>
-            @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-      </div><br />
-    @endif
+  
+    
     <form method="post" action="{{ route('novousuario.store') }}">
           <div class="form-group">
               @csrf
